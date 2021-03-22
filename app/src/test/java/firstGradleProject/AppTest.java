fa0dc41b596abe3995e36c402e9eb8b126569638
laetscbe <laetsce@students.zhaw.ch>
2021-03-22 16:37:53 +0100

    ready for gitHub

--- app/src/test/java/firstGradleProject/AppTest.java
+++ app/src/test/java/firstGradleProject/AppTest.java
@@ -3,10 +3,16 @@
  */
 package firstGradleProject;
 
+// import static spark.Spark.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
 class AppTest {
+    public String getGreeting() {
+        return "Hallo Welt";
+    }
     @Test void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
